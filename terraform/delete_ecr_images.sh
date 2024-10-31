1ccbfb949694816b696d0664e408253481834c53
Charleen Chiu <charleenchiu@msn.com>
2024-10-31 16:09:12 +0800

    fix: delete_ecr_images.sh修正為刪public repo的image

--- terraform/delete_ecr_images.sh
+++ terraform/delete_ecr_images.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# 設定 AWS region
+region="us-east-1"
+
 # 將所有要處理的 repository 儲存到一個陣列中
 repositories=("k8s-shopping-site" "k8s-shopping-site/payment_service" "k8s-shopping-site/product_service" "k8s-shopping-site/order_service" "k8s-shopping-site/user_service")
 
@@ -7,8 +10,8 @@ repositories=("k8s-shopping-site" "k8s-shopping-site/payment_service" "k8s-shopp
 for repo in "${repositories[@]}"; do
     echo "Processing repository: $repo"
 
-    # 取得所有映像的 Digest
-    image_digests=$(aws ecr describe-images --repository-name "$repo" --query 'imageDetails[].imageDigest' --output text 2>/dev/null)
+    # 使用 aws ecr-public 並指定 region 取得所有映像的 Digest
+    image_digests=$(aws ecr-public describe-images --repository-name "$repo" --region "$region" --query 'imageDetails[].imageDigest' --output text 2>/dev/null)
 
     if [ -z "$image_digests" ]; then
         echo "No images found in $repo or repository does not exist."
@@ -20,7 +23,7 @@ for repo in "${repositories[@]}"; do
     # 逐個刪除映像
     for digest in $image_digests; do
         echo "Deleting image with digest: $digest"
-        aws ecr batch-delete-image --repository-name "$repo" --image-ids imageDigest="$digest"
+        aws ecr-public batch-delete-image --repository-name "$repo" --region "$region" --image-ids imageDigest="$digest"
         if [ $? -ne 0 ]; then
             echo "Failed to delete image with digest: $digest from $repo"
         else
